29a81cc090c32b54d4855d94bdb4711fee3af4c9
rudhraio <hey@rudhra.io>
2024-11-24 18:00:29 +0530

    fix: changes in views

--- src/utlis/middleware/request-log.js
+++ src/utlis/middleware/request-log.js
@@ -3,9 +3,9 @@ import { UserActivity } from "../database/models/user-activity.js";
 import logger from "../helpers/logger.js";
 
 async function requestLog(req, res, next) {
-    const ipAddress = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
+    const ipAddress = req.headers['true-client-ip'] || req.ip || req.connection.remoteAddress;
     const userAgent = req.headers['sec-ch-ua-platform'] || req.headers['user-agent'];
-    const visitedUrl = req?.headers?.referer + req?.query?.u || 'Unknown URL';
+    const visitedUrl = req?.headers?.referer + (req?.query?.u || '') || 'Unknown URL';
 
     logger(`[Request] \n[IP]: ${req.ip} 
         \n[method]: ${req.method} 
